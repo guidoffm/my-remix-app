adb3420677388aa29ceb43a21ea54a757ff836ba
guidoffm <guidoffm@users.noreply.github.com>
2024-05-21 17:23:31 +0200

    Add anchor to images

--- app/components/my-image.tsx
+++ app/components/my-image.tsx
@@ -1,7 +1,9 @@
-export default function MyImage({ imageid }: { imageid: string}) {
+export default function MyImage({ imageid }: { imageid: string }) {
     return (
         <div>
-            <img src={`/image/${imageid}`} alt="image-${imageid}" width="100" height="100" />
+            <a href={`/image/${imageid}`} target="_new">
+                <img src={`/image/${imageid}`} alt="image-${imageid}" width="100" height="100" />
+            </a>
         </div>
     );
 }
