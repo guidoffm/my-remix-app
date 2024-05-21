fcd135f993ca66b4ddcae3f36b1c8df49bcda5fc
guidoffm <guidoffm@users.noreply.github.com>
2024-05-21 21:27:16 +0200

    Update my-image.tsx

--- app/components/my-image.tsx
+++ app/components/my-image.tsx
@@ -1,7 +1,7 @@
 export default function MyImage({ imageid }: { imageid: string }) {
     return (
         <div style={{flexBasis: '16.66%', padding: '10px', boxSizing: 'border-box'  }}>
-            <a href={`/image/${imageid}`} target="_new">
+            <a href={`/image/${imageid}`} target="_blank">
                 <img src={`/image/${imageid}`} alt="image-${imageid}" style={{ width: '100%', height: '100%' }} />
             </a>
         </div>
