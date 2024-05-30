e574de2c45d87cdfa93ee6f90216fb88aa5b2cd6
guidoffm <guidoffm@users.noreply.github.com>
2024-05-30 20:02:43 +0200

    Update mypage

--- app/components/upload-form.tsx
+++ app/components/upload-form.tsx
@@ -1,14 +1,18 @@
 export default function UploadForm() {
     return (
-        <div className="w-full max-w-xs">
-            <h1 className="upload-header">Upload</h1>
-            <form method="post" encType="multipart/form-data" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <input type="file" name="avatar" 
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username"
-           />
-                <button type="submit" 
-                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
-                        >Upload</button>
+        <div className="w-full max-w-xl mt-2">
+            <form method="post" encType="multipart/form-data" className="bg-gray-100 shadow-md rounded px-8 pt-8 pb-8 mb-4">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="avatar">
+                        Choose a file
+                    </label>
+                    <input type="file" name="avatar"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                </div>
+                <button type="submit"
+                    className={`w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+                >Upload</button>
             </form>
         </div>
     );
