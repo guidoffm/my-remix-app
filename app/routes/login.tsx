20cb5b4b8e49af08c296bc4bd1d120d880339011
guidoffm <guidoffm@users.noreply.github.com>
2024-06-12 00:11:08 +0200

    Update login form

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -72,24 +72,30 @@ export default function Login() {
     const { error } = useLoaderData<typeof loader>();
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f0f0f0' }}>
+        <div className="flex justify-center items-center h-screen">
             {error ? <div className="error">{error}</div> : null}
-            <form action="/login" method="post" style={{ display: 'flex', flexDirection: 'column', gap: '1rem', padding: '2rem', backgroundColor: '#fff', borderRadius: '8px', boxShadow: '0 0 10px rgba(0,0,0,0.1)' }}>
-                <div>
-                    <p>Please sign in</p>
+            <form action="/login" method="post" className="flex flex-col w-1/3 bg-orange-100 mt-6">
+                <div className="p-4">
+                    <p className="font-bold">Please sign in</p>
                 </div>
-                <label style={{ display: 'flex', flexDirection: 'column' }}>
-                    Username: <input 
-                    type="text" 
-                    name="username" 
-                    autoFocus
-                    style={{ padding: '0.5rem', borderRadius: '4px', border: '1px solid #ccc' }} />
+                <label className="p-4">
+                    Username: <input
+                        type="text"
+                        name="username"
+                        autoFocus className="m-4 input input-bordered input-primary w-full max-w-xs" />
+
                 </label>
-                <label style={{ display: 'flex', flexDirection: 'column' }}>
-                    Password:{" "}
-                    <input type="password" name="password" style={{ padding: '0.5rem', borderRadius: '4px', border: '1px solid #ccc' }} />
+                <label className="p-4">
+                    Password:
+                    <input
+                        type="password"
+                        name="password"
+                        className="m-4 input input-bordered input-primary w-full max-w-xs" />
                 </label>
-                <button type="submit" style={{ padding: '0.5rem 1rem', borderRadius: '4px', border: 'none', backgroundColor: '#007BFF', color: '#fff', cursor: 'pointer' }}>Sign in</button>
+                <div className="p-4">
+                    <button type="submit" className="btn btn-primary w-full">Sign in</button>
+                </div>
+
             </form>
         </div>
     );
