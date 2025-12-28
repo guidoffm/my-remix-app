590f58c656537544587ba28fb63e57c1ea56c864
Guido Müller (Guido Mueller) <8253453+guidoffm@users.noreply.github.com>
2025-12-28 18:45:24 +0100

    Update singleFetch auf false

--- remix.config.mjs
+++ remix.config.mjs
@@ -4,7 +4,7 @@ export default {
     v3_fetcherPersist: true,
     v3_lazyRouteDiscovery: true,
     v3_relativeSplatPath: true,
-    v3_singleFetch: true,
+    v3_singleFetch: false,
     v3_throwAbortReason: true,
   },
 };
